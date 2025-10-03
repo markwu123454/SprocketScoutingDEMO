f4d0b99bc961cb11edb37fcabbe17428ef5b2639
markwu123454 <139573640+markwu123454@users.noreply.github.com>
2025-10-03 08:12:13 -0700

    Stuff

--- DEMOFRONTEND/vite.config.ts
+++ DEMOFRONTEND/vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
             workbox: {
                 navigateFallback: '/offline.html',
                 globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
             },
         }),
     ],
